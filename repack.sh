707174069b78bb9d1fbc4fb7963ea952d435d171
Nvertigo <nvertigo67@gmail.com>
2014-11-04 10:48:58 +0100

    setup

---
+++ repack.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+
+UPDATERSRIPT="META-INF/com/google/updater-script"
+
+if [ -z "$1" ]
+  then
+    echo "Must specify rom.zip"
+    exit 1
+fi
+
+ROMZIP=$1
+
+if [ -f $ROMFILE]
+  then
+    echo "$ROMFILE does not exist"
+    exit 2
+fi
+
+unzip $ROMFILE $UPDATERSCRIPT
+for pf in patches/*.patch; do
+    patch <$pf
+done
+
+zip -r $ROMFILE $UPDATERSCRIPT
+
+rm -r META-INF
+META-INF/com/google/android/updater-script
